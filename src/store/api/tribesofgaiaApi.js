ce6c6927c845eecbc6f7260059a7b5a752273157
Juan Pedro Tovar <jptovar92@gmail.com>
2023-01-05 20:33:11 -0300

    Ups, sory

--- src/store/api/tribesofgaiaApi.js
+++ src/store/api/tribesofgaiaApi.js
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// url: "https://tribesofgaia-api.deta.dev/validate",
-// url: "http://localhost:5000/validate",
+// url: "https://tribesofgaia-api.deta.dev/",
+// url: "http://localhost:5000/",
 
 export const tribesofgaiaApi = createApi({
 	reducerPath: "tribesofgaiaApi",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "https://tribesofgaia-api.deta.dev/validate",
+		baseUrl: "https://tribesofgaia-api.deta.dev/",
 	}),
 	endpoints: (builder) => ({
 		loginAuth: builder.mutation({
