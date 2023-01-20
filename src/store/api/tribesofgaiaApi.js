2698020c539ab6c25f1ef420072380a928e31c21
Juan Pedro Tovar <jptovar92@gmail.com>
2023-01-20 15:56:17 -0300

    URL Correction

--- src/store/api/tribesofgaiaApi.js
+++ src/store/api/tribesofgaiaApi.js
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const tribesofgaiaApi = createApi({
 	reducerPath: "tribesofgaiaApi",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "http://localhost:5000/",
+		baseUrl: "https://tribesofgaia-api.deta.dev/",
 	}),
 	endpoints: (builder) => ({
 		loginAuth: builder.mutation({
